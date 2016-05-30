faa0a04ef6a2ae7b050d93f51645c24b29d6d22f
mslota <mslota@LAP065.prointegra.corp>
2016-05-30 19:34:25 +0200

    Change-Id: I135f447afc692fc5c91d251d5a406e2988785476

--- MortgageCalculator/src/test/java/kolokwium/c/pl/edu/us/CurrencyTest.java
+++ MortgageCalculator/src/test/java/kolokwium/c/pl/edu/us/CurrencyTest.java
@@ -2,17 +2,27 @@ package kolokwium.c.pl.edu.us;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class CurrencyTest
 {
+	private Currency currency;
+	Currency currencyMock;
+	
+	@Before
+	public void setup(){
+		
+		
+	}
+	
 	@Test
 	public void convertFromPlnTest() {
-		fail("Not yet implemented");
+		assertEquals(0.4,currency.convertFromPLN(4),0.001);
 	}
 	
 	@Test
 	public void convertToPlnTest() {
-		fail("Not yet implemented");
+		assertEquals(50,currency.convertToPLN(4),0.001);
 	}
 }
